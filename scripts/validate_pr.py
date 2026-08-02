4ad3ca71af413aa231a31c501593f1c221fac2db
ivallemol <ivallemol@gmail.com>
2026-08-02 13:31:03 -0500

    Update

--- scripts/validate_pr.py
+++ scripts/validate_pr.py
@@ -22,6 +22,41 @@ def get_pr_info():
         github_env = os.getenv('GITHUB_ENV', '/github/env')
         print(f"[LOG] OWNER: {owner}, REPO: {repo}, PR_NUMBER: {pr_number}")
         
+        if not all([owner, repo, pr_number, token]):
+            raise Exception("Faltan variables de entorno necesarias")
+            
+        url = f"https://api.github.com/repos/{owner}/{repo}/pulls/{pr_number}"
+        headers = {'Authorization': f'{token}'}
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        pr_data = response.json()
+        print("[LOG] PR data obtenida correctamente")
+        
+        title = pr_data.get('title', '')
+        body = pr_data.get('body', '')
+        created_at = pr_data.get('created_at')
+        number = pr_data.get('number')
+        branch_name = pr_data.get('head', {}).get('ref','')
+        print(f"[LOG] Titulo: {title}")
+        print(f"[LOG] Rama de origen: {branch_name}")
+        print(f"[LOG] Fecha de creacion: {created_at}")
+        print(f"[LOG] Body: {body}")
+        
+        titulo_valido = bool(re.match(r'^D-\d{5,8}', title))
+        print(f"[LOG] ¿Titulo valido?: {titulo_valido}")
+        
+        ticket_match = re.search(r'- \*\*Ticket\*\*: \*{0,3}((?:APIT-\d{4,8})|(?:UBAG-\d{1,9}))\*{0,3}', body)
+        ticket_code = ticket_match.group(1) if ticket_match else None
+        ticket_valido = bool(ticket_code)
+        print(f"[LOG] ¿Ticket valido?: {ticket_valido}")
+        
+        # Exportar 
+        if github_env:
+            with open(github_env, 'a') as env_file:
+                env_file.write(f'INITIATIVE_CODE={title}\n')
+                env_file.write(f'TICKET_CODE={ticket_code if ticket_code else ""}\n')
+            print("[LOG] Variable de entorno INITIATIVE_CODE y TICKET_CODE exportadas a GITHUB_ENV")
+        
     except Exception as error:
         print(f"[ERROR] {error}")
         sys.exit(1)
