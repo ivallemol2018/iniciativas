fcab1b663f78ffeb2c1cf5a7ccf1abcf88e01594
ivallemol <ivallemol@gmail.com>
2026-08-02 03:24:04 -0500

    Update

---
+++ scripts/create_repos.py
@@ -0,0 +1,119 @@
+import os
+import requests
+import time
+import sys
+import json
+import base64  # <-- Agregado para codificacion base64
+
+# Entradas desde variables de entorno
+token = os.getenv("GITHUB_TOKEN")
+org_name = os.getenv("GITHUB_OWNER")
+repo_info_raw_b64 = os.getenv("REPOS_TO_CREATE","")
+repo_name_pr = os.getenv("GITHUB_REPO")
+repo_creation_origin = os.getenv("INITIATIVE_CODE","").upper()
+
+# Decodifica base64
+repo_info_raw = base64.b64decode(repo_info_raw_b64).decode() if repo_info_raw_b64 else "[]"
+try:
+    repo_info_array = json.loads(repo_info_raw)
+except json.JSONDecodeError:
+    raise Exception("La variable repo_info_array no contiene un JSON valido.")
+
+if not all([token, org_name, repo_info_array]):
+    raise Exception("Faltan variables de entorno requeridas")
+    
+template_map = {
+    "rest": "api-rest-template-repository",
+    "event-driven": "asyncapi-template-repository",
+    "graphql": "api-graphql-template-repository"
+}
+
+headers = {
+    "Authorization": f"Bearer {token}",
+    "Accept": "application/vnd.github+json",
+    "X-GitHub-Api-Version": "2022-11-28"
+}
+
+# --- Flujo principal ---
+repos_creados = []
+repos_no_creados = []
+
+for repo_info in repo_info_array:
+    repo_name = repo_info["repo"]
+    api_type = repo_info["api_type"]
+    api_style = repo_info["api_style"]
+    api_exposure = repo_info["api_exposure"]
+    repo_owner = repo_info.get("owner","")
+    repo_name_lower = repo_name.lower()
+    # Definicion de prefijos
+    external_prefixes = ("private-","public-","open-")
+    internal_prefixes = ("channel-","business-","core-","data-")
+    
+    repo_owner_upper = repo_owner.upper()
+    
+    SCHEMA_PILOT_OWNERS = {
+        "APTI",
+        "TTIB",
+        "APSA",
+        "SIAT",
+        "SCFI",
+        "NTEL"
+    }
+    
+    if repo_name_lower.startswith(external_prefixes):
+        template_repo = "api-rest-template-repository"
+    elif repo_name_lower.startswith(internal_prefixes)
+        if repo_owner_upper in SCHEMA_PILOT_OWNERS:
+            template_repo = "api-rest-template-repository"
+        else:
+            template_repo = "api-rest-internal-template-repository"
+    else:
+        template-repo = template_map.get(api_style)
+        
+    
+    final_name = repo_name
+    github_link = f"https://github.com/{org_name}/{final_name}"
+    api_check_url = f"https://api.github.com/repos/{org_name}/{final_name}"
+    check_reponse = requests.get(api_check_url, headers=headers)
+    
+    if check_reponse.status_code == 200:
+        print(f"El repositorio '{final_name}' ya existe. Se verificara el team developer asignado.")
+        continue
+    
+    # --- Creacion de repositorio ---
+    print(f"Creando repositorio '{final_name}' desde template '{template_repo}'...")
+    generate_url = f"https://api.github.com/repos/{org_name}/{template-repo}/generate"
+    payload_template = {
+        "owner": org_name,
+        "name": final_name,
+        "description": "Repositorio de API",
+        "private": True,
+        "include_all_branches": False
+    }
+    
+    response_template = request.post(generate_url, headers=headers,json=payload_template)
+    repo_link= f"https://github.com/{org_name}/{final_name}"
+    
+    if response_template.status_code == 201:
+        print(f"Repositorio {final_name} creado a partir de template {template_repo} exitosamente.")
+        repos_creados.append({
+            "repo": final_name,
+            "owner": repo_owner,
+            "link": repo_link
+        })
+        with open("repos_creados.txt", "a") as f:
+            f.write(f"{final_name},{repo_owner}\n")
+    else:
+        print(f"Error al crear el repositorio {final_name}: {response_template.status_code}")
+        repos_no_creados.append({
+            "repo": final_name,
+            "owner": repo_owner,
+            "link": repo_link
+        })
+        continue
+        
+     time.sleep(30)   
+        
+    
+    
+    
---
+++ scripts/generate_gat.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+set -o pipefail
+
+# Variables necesarias
+client_id = $APP_ID
+PRIVATE_KEY=$PRIVATE_KEY
+ORGANIZATION=$ORGANIZATION
+
+# Imprimir el APP_ID para depuracion
+echo "APP_ID: $client_id"
+
+# Verificar si la clave privada esta presente
+if [ -z "$PRIVATE_KEY"]; then
+	echo "Private key is missing."
+	exit 1
+fi
+
+#Generar JWT
+now=$(date +%s)
+iat=$((${nom} - 60)) 
+exp=$((${nom} + 600))
+
+b64enc() { openssl base64 -e -A | tr -d '=' | tr '/+' '_-' | tr -d '\n'; }
+
+header_json='{
+	"typ": "JWT",
+	"alg": "RS256"
+}'
+
+header=$(echo -n "${header_json}" | b64enc)
+
+payload_json="{
+	\"iat\":${iat}
+	\"exp\":${exp}
+	\"iss\":\"${client_id}\"
+}"
+
+payload=$(echo -n "${payload_json}" | b64enc)
+
+
+header_payload="${header}.{payload}"
+signature=$(echo -n "$header_payload" | openssl dgst -sha256 -sign <echo -n "$PRIVATE_KEY") | b64enc)
+
+
+jwt_token="${header_payload}.${signature}"
+
+
+installations=$(curl -s -H "Authorization: Bearer $jwt_token" -H "Accept: application/vnd.github.v3+json" https://api.github.com/app/installations)
+
+
+installation_id=$(echo "$installations" | jq -r -arg org "$ORGANIZATION" '.[] | select(.account.login == $org) | .id')
+
+
+if [ -z "$installation_id" ]; then
+	echo "No matching account found for the provided organization."
+	exit 1
+fi
+
+
+
+access_token=$(curl -s -X POST -H "Authorization: Bearer $jwt_token" -H "Accept: application/vnd.github.v3+json" https://api.github.com/app/installations/$installation_id/access_tokens | jq -r '.token' )
+
+if [ -z "$access_token"]; then
+	echo "Failed to obtain access token"
+	exit 1
+fi
+
+
+echo "ACCESS_TOKEN=$access_token" >> $GITHUB_ENV
+
+
+if [-n "$1" ]; then
+	output_name = "$1"
+	echo "$output_name=$access_token" >> "$GITHUB_OUTPUT"
+fi
+
+
+echo "ID de installation seleccionado_: $installation_id"
+
+
+
