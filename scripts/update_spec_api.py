fc813510ce9aa18b902f3f61051334c8eaece0ee
ivallemol2018 <42480842+ivallemol2018@users.noreply.github.com>
2026-09-06 21:55:06 -0500

    Enhance logging for API type handling
    Added logging for API type processing in update_spec_api.py.

--- scripts/update_spec_api.py
+++ scripts/update_spec_api.py
@@ -896,9 +896,11 @@ def main():
 
     for api_name, grupo in df.groupby('API', sort=False):
         api_type = str(grupo.iloc[0]['Tipo']).strip()
+        logging.info(f"Api type {api_type}")
         if api_type == 'PQL' or api_type == 'PU' or api_type == 'OP' :
             continue
 
+        logging.info("fin Api type")
         parsed = parse_api_name(str(api_name).strip(), api_type)
         if not parsed or parsed['repo_name'] not in repos_creados:
             continue
